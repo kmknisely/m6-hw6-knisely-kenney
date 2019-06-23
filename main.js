1210649c8bf30ae573fbd782ea00d134978b9ce9
Kenney Knisely <kknisely@mac.com>
2019-06-23 19:28:41 -0400

    Add JS to create the lunch generator

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 var button = document.getElementsByTagName('button')[0];
 var myHeading = document.getElementsByTagName('h1')[0];
 
+var ourRandomLunchItem = document.createElement('p');
+var lunchItemWrapper = document.getElementById('generator-results');
+lunchItemWrapper.appendChild(ourRandomLunchItem);
+
+var placeholderText = document.createTextNode('');
+ourRandomLunchItem.appendChild(placeholderText);
+
+var lunchItems = [
+  'Mexican Food',
+  'Sushi',
+  'Chicken Wings',
+  'Pizza',
+  'Greek Food',
+  'Subs or Sandwhiches',
+  'Ramen',
+  'Pasta',
+  'Chinese Food',
+  'Salad',
+  'Charcuterie',
+  'Burger',
+];
+
 var myFunction = function() {
-  myHeading.classList.add('big-and-bold')
+  lunchItems.sort(function(a, b){return 0.5 - Math.random()});
+  var ourRandomLunchItemFiller = document.createTextNode(lunchItems[0]);
+  ourRandomLunchItem.appendChild(ourRandomLunchItemFiller);
+  var ourButtonSection = document.getElementById('generator-button');
+  ourButtonSection.removeChild(button);
+  ourRandomLunchItem.classList.add('big-and-bold');
 
 };
 
